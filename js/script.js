5fb8895b311c5a44215b46d9591e46cd54902da6
Flavia <flaviamatta90@gmail.com>
2020-08-03 15:56:18 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+// chiedi il nome all'utente
+var nome = prompt("Come ti chiami?")
 
+// chiedi il suo cognome
+var cognome = prompt("Qual è il tuo cognome?")
+
+// chiedi il suo colore preferito
+var colore = prompt("Qual è il tuo colore preferito?")
+
+// stampa sulla pagina nomecognomecolorepreferito19
+document.getElementsByClassName('nomecognomecolore').innerHTML="nome" + "cognome" + "colore" + "19";
+
+// alert("nome" + "cognome" + "colore" + "19");
